cfea2c4649d796516e842256f416d72d0a253e03
raz4r1 <razer998@interia.pl>
2015-02-17 18:07:38 +0100

    Główna klasa pluginu

--- src/pl/raz4r/permiess/Main.java
+++ src/pl/raz4r/permiess/Main.java
@@ -3,6 +3,8 @@ package pl.raz4r.permiess;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import pl.raz4r.permiess.listeners.PlayerJoin;
+
 public class Main extends JavaPlugin {
 	
 	public static FileConfiguration getConfig;
@@ -13,6 +15,10 @@ public class Main extends JavaPlugin {
 	{
 		System.out.println("[PERMIESS] Uruchamianie pluginu");
 		System.out.println("[PERMIESS] Wczytywanie configu");
+		getConfig=this.getConfig();
+		System.out.println("[PERMIESS] Config wczytany");
+		System.out.println("[PERMIESS] Uruchamianie wtyczek");
+		getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
 		getPlugin=this;
 		getConfig=this.getConfig();
 	}
