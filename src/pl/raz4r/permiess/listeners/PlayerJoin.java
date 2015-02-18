f3f68e34b2b4393df689f179e4f98737554f6b63
raz4r1 <razer998@interia.pl>
2015-02-18 20:07:33 +0100

    #

--- src/pl/raz4r/permiess/listeners/PlayerJoin.java
+++ src/pl/raz4r/permiess/listeners/PlayerJoin.java
@@ -1,8 +1,21 @@
 package pl.raz4r.permiess.listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import pl.raz4r.permiess.Config;
 
 public class PlayerJoin implements Listener {
 
-	
+	@EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+		if(Config.getConfig("Perm").contains("Players." + e.getPlayer().getName()));
+		{
+			
+		}
+		else{
+			
+		}
+	}
 }
