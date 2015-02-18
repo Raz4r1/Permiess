78335762b792c23998f2a89b201640110d8bc37a
raz4r1 <razer998@interia.pl>
2015-02-18 22:16:36 +0100

    #

--- src/pl/raz4r/permiess/data/DataConfig.java
+++ src/pl/raz4r/permiess/data/DataConfig.java
@@ -6,11 +6,21 @@ public class DataConfig {
 
 	public static String getPierwszaWiadomoscPowitalna()
 	{
-		return Config.getConfig("Config").getString("Gerneral.FirstWiadomoscPowitalna");
+		return Config.getConfig("Config").getString("General.FirstWiadomoscPowitalna");
+	}
+	public static String getFormatWiadomosci()
+	{
+		return Config.getConfig("Config").getString("General.FormatWiadomosci");
 	}
 	public static void setPierwszaWiadomoscPowitalna(String w)
 	{
-		Config.getConfig("Config").set("Gerneral.FirstWiadomoscPowitalna",w);
+		Config.getConfig("Config").set("General.FirstWiadomoscPowitalna",w);
 		Config.save("Config");
 	}
+	public static void setFormatWiadomosci(String w)
+	{
+		Config.getConfig("Config").set("General.FormatWiadomosci", w);
+		Config.save("Config");
+	}
+	
 }
--- src/pl/raz4r/permiess/listeners/AsyncPlayerChat.java
+++ src/pl/raz4r/permiess/listeners/AsyncPlayerChat.java
@@ -4,10 +4,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import pl.raz4r.permiess.data.DataConfig;
+import pl.raz4r.permiess.data.DataPerm;
+
 public class AsyncPlayerChat implements Listener {
 	@EventHandler
-	public boolean onChat(AsyncPlayerChatEvent e) {
-		return false;
-		
+	public void onChat(AsyncPlayerChatEvent e) {
+		e.setFormat(DataConfig.getFormatWiadomosci().toString().replace("%NICK%", e.getPlayer().getName().replace("%GRUPA%", DataPerm.getGroupChatPrefi(DataPerm.getGroupPlayer(e.getPlayer().getName())))));
+
 	}
 }
